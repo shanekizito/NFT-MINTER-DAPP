6b56dfaed57d88562fd90902c8cdc1dc4ce51694
shanekizito <shaneyoungk@gmail.com>
2021-11-19 23:00:14 +0300

    first commit

---
+++ Client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import NavBar from './Components/NavBar';
+import './App.css';
+import Home from './Pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+ 
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path='/' exact element={<Home/>} />
+           
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
